7b278a1530c381bee660c7e16e2aadfd8fad6985
Tomas <tomasbudassi@gmail.com>
2022-04-21 18:55:48 -0300

    create api

---
+++ src/main/java/com/uda/gestorreclamos/GestorReclamosApplication.java
@@ -0,0 +1,13 @@
+package com.uda.gestorreclamos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorReclamosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorReclamosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uda/gestorreclamos/GestorReclamosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uda.gestorreclamos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorReclamosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
