1734eed3ca5bd49bc5c33ab17822017083a23444
Tomas <tomasbudassi@gmail.com>
2022-05-15 21:06:26 -0300

    Se fixeo el update de reclamos

--- src/main/java/com/uda/gestorreclamos/services/IssueService.java
+++ src/main/java/com/uda/gestorreclamos/services/IssueService.java
@@ -2,6 +2,7 @@ package com.uda.gestorreclamos.services;
 
 import com.uda.gestorreclamos.dtos.IssueRequestDTO;
 import com.uda.gestorreclamos.dtos.IssueResponseDTO;
+import com.uda.gestorreclamos.dtos.IssueUpdateRequestDTO;
 import com.uda.gestorreclamos.models.Issue;
 
 import java.util.List;
@@ -18,7 +19,7 @@ public interface IssueService {
 
     Issue getById(Integer id);
 
-    Issue update(Integer id, IssueRequestDTO issue) throws Exception;
+    Issue update(Integer id, IssueUpdateRequestDTO issue) throws Exception;
 
     void deleteById(Integer id);
 
