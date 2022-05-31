66738541c2a2c341e82d252bb29994dcde8d39d7
tomasbudassi <54443823+tomasbudassi@users.noreply.github.com>
2022-05-30 22:01:30 -0300

    PR 12 (#12)

--- src/main/java/com/uda/gestorreclamos/repositories/IssueRepository.java
+++ src/main/java/com/uda/gestorreclamos/repositories/IssueRepository.java
@@ -1,6 +1,7 @@
 package com.uda.gestorreclamos.repositories;
 
 import com.uda.gestorreclamos.models.Issue;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,9 +9,12 @@ import java.util.List;
 
 @Repository
 public interface IssueRepository extends CrudRepository<Issue, Integer> {
-    List<Issue> findByStatusNot(String status);
+    List<Issue> findByStatusNot(String status, Pageable pageable);
 
-    List<Issue> findByStatusContainsIgnoreCase(String status);
+    List<Issue> findByStatusContainsIgnoreCase(String status, Pageable pageable);
 
     List<Issue> findByEmployeeId(Integer id);
+
+    List<Issue> findAll(Pageable pageable);
+
 }
